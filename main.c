1e6a43b64e56c30d21b0d7c116be71cd35c866a2
naim <naim2206@yahoo.com.mx>
2021-11-21 18:20:39 -0600

    ya se gana cuando mata a rey

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "ajedrez.h"
 #include <stdlib.h>
 
-//https://github.com/austin0209/just-chess
-// peon 1, alfil 2, caballo 3, torre 5, Dama 9, rey 100, nada 0, negativo si es negro;
+// peon 1, alfil 2, caballo 3, torre 5, Dama 9, rey 100, nada 0, 
+//negativo si es negro;
 //que piezas estan en que lugar
 int board_pieces[8][8] = {
     {-5,-3,-2,-9,-100,-2,-3,-5},
@@ -17,21 +17,6 @@ int board_pieces[8][8] = {
 };
 
 
-// identificar los lugares en el tablero
-/*int board_values[8][8] = {
-    {11, 12, 13, 14, 15, 16, 17, 18},
-    {21, 22, 23, 24, 25, 26, 27, 28},
-    {31, 32, 33, 34, 35, 36, 37, 38},
-    {41, 42, 43, 44, 45, 46, 47, 48},
-    {51, 52, 53, 54, 55, 56, 57, 58},
-    {61, 62, 63, 64, 65, 66, 67, 68},
-    {71, 72, 73, 74, 75, 76, 77, 78},
-    {81, 82, 83, 84, 85, 86, 87, 88}
-};*/
-
-// 0 negros, 1 blancos
-
-
 int main()
 {
     InitWindow(SCREAN_WIDTH, SCREAN_HEIGHT, "Chess");
@@ -39,33 +24,35 @@ int main()
     SetTargetFPS(60);
 
     Player* p = newPlayer();
-    //Game* g = newGame();
+    Game* g = newGame();
     myTexture* t = loadTextures();
-    int band = 0;
+    int whoWon = 0;
 
     while (!WindowShouldClose())
     {
-
         BeginDrawing();
-        drawBoard(board_pieces);
-        drawPieces(board_pieces, t);
-        // pa debuggear, eliminar despues
-        char pr[10];
-        itoa(band, pr, 10);
-        if ((IsMouseButtonDown(MOUSE_RIGHT_BUTTON)))
+        if (whoWon == 0)
         {
-            DrawText(pr, 10, 10, 20, BLUE);
+            drawBoard(board_pieces);
+            drawPieces(board_pieces, t);
         }
-        // hasta aqui
         //falta revisar lo de los turnos
+        makeMove(g, p, board_pieces);
+
+        // pa debuggear
+        //char pr[10];
+        //itoa(whoWon, pr, 10);
+        //if ((IsMouseButtonDown(MOUSE_RIGHT_BUTTON)))
+        //    DrawText(pr, 10, 10, 20, BLUE);
+        // hasta aqui
+
+        whoWon = checkWin(board_pieces);
+        if (whoWon != 0)
+            showWinner(whoWon);
         EndDrawing();
-        makeMove(&band, p, board_pieces);
-
-
     }
     CloseWindow();
 
     return 0;
-
 
 }
